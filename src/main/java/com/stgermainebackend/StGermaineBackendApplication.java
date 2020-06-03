108589f827836fa933a6f3117f34ab1f1c0cc7a8
jagardner21 <42302459+jagardner21@users.noreply.github.com>
2020-06-02 22:03:12 -0700

    first commit

---
+++ src/main/java/com/stgermainebackend/StGermaineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stgermainebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StGermaineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StGermaineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stgermainebackend/StGermaineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stgermainebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StGermaineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
